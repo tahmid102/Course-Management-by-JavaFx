b30d3454c3b093eefbcbcccbb10232541f65d5d1
Tahmid <mdtahmidulislam102@gmail.com>
2025-06-26 20:56:32 +0600

    ThamidPussh

--- src/main/java/src/drimjavafxproject/StudentList.java
+++ src/main/java/src/drimjavafxproject/StudentList.java
@@ -1,5 +1,34 @@
 package src.drimjavafxproject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentList {
+    private List<Student> students;
+    public StudentList(){
+        students=new ArrayList<>();
+    }
+    public void addStudent(Student a){
+        students.add(a);
+    }
+    public boolean isStudentAvailable(Student b){
+        for(Student a:students){
+            if(a.studentId==b.studentId){
+                return true;
+            }
+        }
+        return false;
+    }
+    public void removeStudent(Student a){
+        students.remove(a);
+    }
+    public Student searchStudent(int userId){
+        for(Student a:students){
+            if(a.studentId==userId){
+                return a;
+            }
+        }
+        return null;
+    }
 
 }

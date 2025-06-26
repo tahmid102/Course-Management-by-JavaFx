493227f556754237fafe49102e77e565851221c1
Tahmid <mdtahmidulislam102@gmail.com>
2025-06-27 02:44:14 +0600

    ThamidPussh

---
+++ src/main/java/src/drimjavafxproject/DashboardController.java
@@ -0,0 +1,4 @@
+package src.drimjavafxproject;
+
+public class DashboardController {
+}
--- src/main/java/src/drimjavafxproject/LoginController.java
+++ src/main/java/src/drimjavafxproject/LoginController.java
@@ -2,8 +2,13 @@ package src.drimjavafxproject;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Locale;
 
 public class LoginController {
@@ -18,6 +23,9 @@ public class LoginController {
         public Label welcomeLabel;
         @FXML
         public ComboBox<String> roleBox;
+
+        public Button cancelButton;
+
         Student a=new Student("tahmid",2305180);
         Student b=new Student("mufeed",2305151);
         StudentList A=new StudentList();
@@ -30,30 +38,30 @@ public class LoginController {
                 A.addStudent(a);
                 A.addStudent(b);
 
+
+                usernameField.setOnAction(e-> passwordField.requestFocus());
+                passwordField.setOnAction(e-> submitButton.requestFocus());
+
         }
 
 
 
 
-        public void onSubmit(ActionEvent actionEvent) {
+        public void onSubmit(ActionEvent actionEvent) throws IOException {
                 String username=usernameField.getText().trim().toLowerCase();
                 String password=passwordField.getText();
-                usernameField.setDisable(true);
-                passwordField.setDisable(true);
-                usernameField.setVisible(false);
-                passwordField.setVisible(false);
-                roleBox.setVisible(false);
-                roleBox.setVisible(false);
-//                if (username.equals(A.) && password.equals("1234")) {
-//
-//                        errorLabel.setText("Login successful!");
-//                } else {
-//                        errorLabel.setText("Invalid credentials");
-//                }
+
                 int enteredId=Integer.parseInt(username);
                 if(A.isStudentAvailable(enteredId)){
                         if(A.searchStudent(enteredId).getStudentPassword().equals(password)){
                                 errorLabel.setText("Login successful!");
+                                FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Dashboard.fxml"));
+                                Scene scene=new Scene(fxmlLoader.load(),1000,1000);
+                                Stage stage = (Stage) submitButton.getScene().getWindow();
+                                stage.setScene(scene);
+                                stage.setTitle("Dash");
+                                stage.show();
+
                         }
                         else{
                                 errorLabel.setText("Wrong Password");
@@ -62,7 +70,18 @@ public class LoginController {
                 else{
                         errorLabel.setText("Invalid credentials");
                 }
+                usernameField.setDisable(true);
+                passwordField.setDisable(true);
+                usernameField.setVisible(false);
+                passwordField.setVisible(false);
+                roleBox.setVisible(false);
+                roleBox.setDisable(false);
 
+
+        }
+        public void onCancel(){
+                Stage stage = (Stage) cancelButton.getScene().getWindow();
+                stage.close();
         }
 
 }

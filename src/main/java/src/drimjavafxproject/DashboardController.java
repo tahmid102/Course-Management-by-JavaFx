64e4c5670bdfe7b1e1855ce90d2fa5dd8410a431
Tahmid <mdtahmidulislam102@gmail.com>
2025-06-27 17:25:35 +0600

    ThamidPussh

--- src/main/java/src/drimjavafxproject/DashboardController.java
+++ src/main/java/src/drimjavafxproject/DashboardController.java
@@ -1,4 +1,28 @@
 package src.drimjavafxproject;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
 public class DashboardController {
+    public HBox hboxAll;
+    public Text topText;
+    public VBox vBox;
+    public Button homeButton;
+    public Button myCoursesButton;
+    public Button addCourseButton;
+
+    public void onHomeButton(ActionEvent actionEvent) {
+
+    }
+
+    public void onMyCourses(ActionEvent actionEvent) {
+    }
+
+    public void onAdd(ActionEvent actionEvent) {
+
+    }
+
 }

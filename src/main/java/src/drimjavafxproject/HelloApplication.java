650b42f8cc52f7a2c352c209720e468040fbac96
Tahmid <mdtahmidulislam102@gmail.com>
2025-06-28 20:51:54 +0600

    ThamidPussh

--- src/main/java/src/drimjavafxproject/HelloApplication.java
+++ src/main/java/src/drimjavafxproject/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Tahmid bhai er fanklub");
+        stage.setTitle("mufeeeedeeeeddddddd");
         stage.setScene(scene);
         stage.show();
     }
